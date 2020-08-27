0a889948149afb0fc56e98d7e040d54ac243e5a8
tablu <tablu666@outlook.com>
2020-08-27 10:50:26 +0800

    项目目录结构设置

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+
   }
 }
 </script>
---
+++ src/pages/order.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <nav-header></nav-header>
+        <router-view></router-view>
+        <nav-footer></nav-footer>
+    </div>
+</template>
+<script>
+    import NavFooter from "../components/NavFooter";
+    export default {
+        name:'nav-header',
+        components: {NavFooter}
+    }
+</script>
